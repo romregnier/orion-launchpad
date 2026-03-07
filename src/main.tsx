a2288a8aa6a9b98becfd2516f98ded7f8e2fff0f
Orion 🌟 <romain@rive-studio.com>
2026-03-07 18:33:39 +0000

    feat: Sentry error monitoring intégré

--- src/main.tsx
+++ src/main.tsx
@@ -1,3 +1,12 @@
+import * as Sentry from '@sentry/react'
+Sentry.init({
+  dsn: 'https://bef343a772418a7e6b09fd163fa91ad6@o4511004596633600.ingest.de.sentry.io/4511004637593680',
+  environment: import.meta.env.MODE,
+  integrations: [Sentry.browserTracingIntegration()],
+  tracesSampleRate: 0.1,
+  replaysOnErrorSampleRate: 0,
+})
+
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App'
